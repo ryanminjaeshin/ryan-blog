7c16d7143889235f45abe1de56b6c3f95b15b7da
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-01-31 15:21:55 -0800

    Testing template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <div className="content">
+        <h1>App Component</h1>
+      </div>
     </div>
   );
 }
