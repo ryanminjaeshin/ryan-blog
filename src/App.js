6936017b7f274163cce24e64ddbca14dfd72bf06
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-01-31 15:29:10 -0800

    testing

--- src/App.js
+++ src/App.js
@@ -5,12 +5,20 @@ function App() {
 
   const title = 'Welcome to the new blog';
   const likes = 50;
+  const link = 'http://www.google.com';
 
   return (
     <div className="App">
       <div className="content">
         <h1>{ title }</h1>
         <p>Liked { likes } times </p>
+
+        <p>{ 10 }</p>
+        <p>{'hello, Ryan'}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <p>{ Math.random() * 20}</p>
+
+        <a href={ link }>Google Site</a>
       </div>
     </div>
   );
