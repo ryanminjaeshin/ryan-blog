bf8470a748ff105ebccbe3600a151266dabbe508
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-01-31 15:26:06 -0800

    Add likes template and add some notes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 
 function App() {
+  // we can output numbers, strings, and arrays but not object
+
   const title = 'Welcome to the new blog';
+  const likes = 50;
 
   return (
     <div className="App">
       <div className="content">
         <h1>{ title }</h1>
+        <p>Liked { likes } times </p>
       </div>
     </div>
   );
