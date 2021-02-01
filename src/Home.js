2a66e6ec94002f51bb799fffa760254397afd560
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-01-31 16:31:44 -0800

    Add hooks and setName function

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
 const Home = () => {
     
+    // let name = 'Ryan';
+    const [name, setName] = useState('Ryan');
+
     const handleClick = () => {
+        setName('Minjae');
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{ name }</p>
             <button onClick={ handleClick }>Click me</button>  
         </div>
     );
