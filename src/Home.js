625f6e3e0769b8f760bf15533142bd13f3f42887
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-01-31 16:26:39 -0800

    Add handleClickAgain

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,15 @@ const Home = () => {
         console.log('hello, folks');
     }
 
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e);
+    }
+
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <button onClick={ handleClick }>Click me</button>
+            <button onClick={ handleClick }>Click me</button>  
+            <button onClick={(e) => handleClickAgain('Ryan', e)}>Click me again</button>
         </div>
     );
 }
