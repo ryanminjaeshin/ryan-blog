60694ae282c33778be4efbb2a5606d84e45123a4
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-01-31 17:01:46 -0800

    Passing blogs props to BlogList

--- src/Home.js
+++ src/Home.js
@@ -7,11 +7,11 @@ const Home = () => {
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ])
+      ]);
 
     return (
         <div className="home">
-            <BlogList />
+            <BlogList blogs = {blogs}/>
         </div>
     );
 }
