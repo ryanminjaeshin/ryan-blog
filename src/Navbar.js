23f6ddb2889c6769e8410a47518745e8a91e5d7c
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-01-31 16:50:28 -0800

    Update Navbar title

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1>The Ryan Blog</h1>
+            <h1>Ryan's Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create" style={{
