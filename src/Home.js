721931229df13c3226a656a2f45005eea24cd3a7
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-01-31 16:27:17 -0800

    Remove handleClickAgain

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,12 @@
 const Home = () => {
     
     const handleClick = () => {
-        console.log('hello, folks');
-    }
-
-    const handleClickAgain = (name, e) => {
-        console.log('hello ' + name, e);
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
             <button onClick={ handleClick }>Click me</button>  
-            <button onClick={(e) => handleClickAgain('Ryan', e)}>Click me again</button>
         </div>
     );
 }
