9e065a51447eef5f04bf8400cbec3dc98d5e5f55
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-01-31 16:34:10 -0800

    Add age hook

--- src/Home.js
+++ src/Home.js
@@ -4,15 +4,17 @@ const Home = () => {
     
     // let name = 'Ryan';
     const [name, setName] = useState('Ryan');
+    const [age, setAge] = useState('30')
 
     const handleClick = () => {
         setName('Minjae');
+        setAge(31)
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
-            <p>{ name }</p>
+            <p>{ name } is { age } years old</p>
             <button onClick={ handleClick }>Click me</button>  
         </div>
     );
