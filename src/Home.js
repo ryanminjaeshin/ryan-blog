3e288882fcd435575b8f0734a3d2405e03f21618
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-01-31 16:41:52 -0800

    Add blog hook

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,12 @@ import { useState } from 'react';
 
 const Home = () => {
 
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+      ])
+
     return (
         <div className="home">
         </div>
