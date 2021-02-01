a74ac0e41bd583f94e63128a7c563a12bf528f37
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-01-31 16:47:53 -0800

    Map the blogs

--- src/Home.js
+++ src/Home.js
@@ -10,6 +10,12 @@ const Home = () => {
 
     return (
         <div className="home">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
     );
 }
