747e80434cb5c65c18bed553a0a361aa196487a8
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-01-31 16:40:21 -0800

    Clear Home component

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,9 @@
 import { useState } from 'react';
 
 const Home = () => {
-    
-    // let name = 'Ryan';
-    const [name, setName] = useState('Ryan');
-    const [age, setAge] = useState('30')
-
-    const handleClick = () => {
-        setName('Minjae');
-        setAge(31)
-    }
 
     return (
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old</p>
-            <button onClick={ handleClick }>Click me</button>  
         </div>
     );
 }
