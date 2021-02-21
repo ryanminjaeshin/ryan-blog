87c38ca700fce54201dac3cd2c8dfca45654d36e
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-20 16:17:49 -0800

    Reusing components with filter()

--- src/Home.js
+++ src/Home.js
@@ -12,8 +12,9 @@ const Home = () => {
     return (
         <div className="home">
             <BlogList blogs = {blogs} title = 'All Blogs!'/>
+            <BlogList blogs = {blogs.filter((blog) => blog.author === 'mario' )} title = "Mario's Blogs!"/>
         </div>
     );
 }
- 
+
 export default Home;
