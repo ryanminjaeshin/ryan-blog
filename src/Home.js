df95fc9ba1b54fa1caf8a33134c5a41c24215847
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-20 16:28:49 -0800

    Test useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
@@ -13,6 +13,11 @@ const Home = () => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    // It is going to run every time there is re-render (Anytime, the data gets changed)
+    useEffect(() => {
+        console.log('use effect ran')
+    })
 
     return (
         <div className="home">
