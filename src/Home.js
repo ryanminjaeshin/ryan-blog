f37c0f43e5e542b67d7477fc077f68e749d1b367
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-22 21:17:55 -0800

    Fetch data using useEffect

--- src/Home.js
+++ src/Home.js
@@ -14,13 +14,19 @@ const Home = () => {
 
     // It is going to run every time there is re-render (Anytime, the data gets changed)
     useEffect(() => {
-        console.log('use effect ran')
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then((data) => {
+                console.log(data);
+            })
     }, []);
 
     return (
         <div className="home">
-            <BlogList blogs = {blogs} title = 'All Blogs!' handleDelete = {handleDelete}/>
-            <BlogList blogs = {blogs.filter((blog) => blog.author === 'mario' )} title = "Mario's Blogs!" handleDelete = {handleDelete}/>
+            {/* <BlogList blogs = {blogs} title = 'All Blogs!' handleDelete = {handleDelete}/>
+            <BlogList blogs = {blogs.filter((blog) => blog.author === 'mario' )} title = "Mario's Blogs!" handleDelete = {handleDelete}/> */}
             <button onClick={() => setName('luigi')}>change name></button>
             <p>{name}</p>
         </div>
