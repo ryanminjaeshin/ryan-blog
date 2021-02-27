b25d71e8097fd3cc151e29cae706419e3c4340ba
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-27 15:30:52 -0800

    Fix bugs

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,17 @@
-import { useState, useEffect } from 'react';
-import BlogList from './BlogList';
-import useFetch from './useFetch';
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
-    const {data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs')
-
-    return (
-        <div className="home">
-            {error && <div> { error } </div>}
-            {isPending && <div> Loading... </div>}
-            {blogs && <BlogList blogs = {blogs} title = 'All Blogs!' />}
-            {/* <button onClick={() => setName('luigi')}>change name></button>
-            <p>{name}</p> */}
-        </div>
-    );
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
 
 export default Home;
