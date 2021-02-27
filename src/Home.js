75315abd6e8f9bc781216443d568e77c44fc4806
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-02-26 20:14:29 -0800

    import useFetch to home.js and use it

--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,10 @@
 import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-    //   const [name, setName] = useState('mario');
-
-    // const handleDelete = id => {
-    //     const newBlogs = blogs.filter(blog => blog.id !== id);
-    //     setBlogs(newBlogs);
-    // }
-
-    // It is going to run every time there is re-render (Anytime, the data gets changed)
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                if(!res.ok) {
-                    throw Error('could not fetch the data for that resource')
-                }
-                return res.json();
-            })
-            .then(data => {
-                console.log(data);
-                setBlogs(data);
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err => {
-                setIsPending(false);
-                setError(err.message);
-            })
-    }, []);
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className="home">
