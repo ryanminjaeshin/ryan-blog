591a9a724ec470f0bfd5abbb460026679e36e883
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-02-26 20:05:24 -0800

    Copy useEffect from Home.js

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,3 +1,21 @@
 const useFetch = () => {
-    
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                if(!res.ok) {
+                    throw Error('could not fetch the data for that resource')
+                }
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                setBlogs(data);
+                setIsPending(false);
+                setError(null);
+            })
+            .catch(err => {
+                setIsPending(false);
+                setError(err.message);
+            })
+    }, []);
 }
