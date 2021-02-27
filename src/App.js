4d27f558aaecdeaf911326576c0c0c4fb6808137
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-27 15:14:37 -0800

    Remove variables

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import Navbar from './Navbar';
 import Home from './Home';
 
 function App() {
-  // we can output numbers, strings, and arrays but not object
-
-  // const title = 'Welcome to the new blog';
-  // const likes = 50;
-  // const link = 'http://www.google.com';
 
   return (
     <div className="App">
