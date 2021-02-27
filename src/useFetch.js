db0f8297f978b05369fb19bf65882f2e8a2779fa
ryanminjaeshin <ryanminjaeshin@gmail.com>
2021-02-26 20:11:57 -0800

    Add url as an argument

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,13 +1,14 @@
 import { useState, useEffect } from 'react';
 
-const useFetch = () => {
+// you can add url endpoint so we can use it in different components'
+const useFetch = (url) => {
 
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        fetch(url)
             .then(res => {
                 if(!res.ok) {
                     throw Error('could not fetch the data for that resource')
@@ -24,7 +25,8 @@ const useFetch = () => {
                 setIsPending(false);
                 setError(err.message);
             })
-    }, []);
+            // whenever url gets changed, useEffect function will be ran'
+    }, [url]);
 
     return { data, isPending, error }
 }
