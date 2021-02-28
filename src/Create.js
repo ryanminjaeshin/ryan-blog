d617e409ccd508735700365241c9385eed3a8d08
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-27 18:56:13 -0800

    Add form

--- src/Create.js
+++ src/Create.js
@@ -2,6 +2,22 @@ const Create = () => {
   return (
     <dlv className="create">
       <h2>Add a New Blog</h2>
+      <form>
+        <label>Blog Title:</label>
+        <input
+          type="text"
+          required
+        />
+        <label>Blog body:</label>
+        <textarea
+          required
+        />
+        <label>Blog author:</label>
+        <select>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+      </form>
     </dlv>
   );
 }
