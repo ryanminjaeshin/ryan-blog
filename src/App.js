5d81f57a62091edb85baa5936a5c856c3a15e1f6
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-28 15:44:14 -0800

    Add NotFound

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Navbar';
 import Home from './Home';
 import Create from './Create';
 import BlogDetails from './BlogDetails'
+import NotFound from './NotFound';
 
 function App() {
 
@@ -21,6 +22,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route>
+              <NotFound />
             </Route>
           </Switch>
         </div>
