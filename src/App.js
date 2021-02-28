b8e26f38cc0c837e417f91b4d0a5edcd96c8d8b6
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-27 16:25:19 -0800

    Add Router in App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Home />
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
