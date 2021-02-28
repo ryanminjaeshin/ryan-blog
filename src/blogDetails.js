666f5008856972b78f88febe34221b402fbe8ff3
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-27 18:24:02 -0800

    Import custom hook

--- src/blogDetails.js
+++ src/blogDetails.js
@@ -1,7 +1,9 @@
 import { useParams } from 'react-router-dom';
+import useFetch from './useFetch';
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs' + id);
   return (
     <div className="blog-details">
       <h2>Blog Details - {id}</h2>
