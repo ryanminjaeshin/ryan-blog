b70582b3d950d184b3b9102565b06e37baeed716
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-28 15:48:46 -0800

    Add path="*" to catch all non-found page

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
             <Route path="/blogs/:id">
               <BlogDetails />
             </Route>
-            <Route>
+            <Route path="*">
               <NotFound />
             </Route>
           </Switch>
