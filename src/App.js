3cd5a79c7590eb3d44e39541ccb1d6d0e0e064e7
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-27 16:31:47 -0800

    Add exact prop to home path

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
             </Route>
             <Route path="/create">
