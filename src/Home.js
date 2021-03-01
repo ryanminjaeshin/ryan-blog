bb2467ae3e29bde642e98750ace3096622a0e84b
Ryan Shin <ryanminjaeshin@gmail.com>
2021-02-28 18:15:57 -0800

    Revert "Update"
    This reverts commit 9ee13b1e8b667d19fa577d44e45811a9d64c4b34.

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,10 @@
+<<<<<<< HEAD
 import BlogList from './BlogList';
 import useFetch from './useFetch';
+=======
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
+>>>>>>> b70582b3d950d184b3b9102565b06e37baeed716
 
 const Home = () => {
   const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
